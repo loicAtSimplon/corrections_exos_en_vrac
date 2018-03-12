6ff0f86fa3914a7e4de9dd4094388289926facf1
LoicDerrieux <l.derrieux@fondespierre.com>
2018-03-12 10:23:00 +0100

    Exercice1

---
+++ src/com/beweb/lunel/programmation/exosEnVrac/algo/Exercice1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.beweb.lunel.programmation.exosEnVrac.algo;
+
+/**
+ *
+ * Les noms des variables sont aussi importants que les valeurs ou les types.
+ * En effet ils donnent une indication sur comment utiliser les informations
+ * contenues.
+ * @author loic
+ */
+public class Exercice1 {
+    
+    /**
+     * Methode invoquée pour lancer l'exercice 1
+     */
+    public static void launch(){
+        boolean empty = false;
+        boolean fully = true;
+    }
+}
