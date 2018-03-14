4371fb8651ecbc5a546432c09892d0efd7f8d65b
ilies <ilies@beweb-16>
2018-03-14 10:11:01 +0100

    Ajout de l'exo 7

--- src/com/beweb/lunel/programmation/exosEnVrac/AlgoMain.java
+++ src/com/beweb/lunel/programmation/exosEnVrac/AlgoMain.java
@@ -8,6 +8,7 @@ package com.beweb.lunel.programmation.exosEnVrac;
 import com.beweb.lunel.programmation.exosEnVrac.algo.Exercice1;
 import com.beweb.lunel.programmation.exosEnVrac.algo.Exercice2;
 import com.beweb.lunel.programmation.exosEnVrac.algo.Exercice3;
+import com.beweb.lunel.programmation.exosEnVrac.algo.Exercice7;
 
 /**
  *
@@ -22,6 +23,7 @@ public class AlgoMain {
         Exercice1.launch();
         Exercice2.launch();
         Exercice3.launch();
+        Exercice7.launch();
         
     }
     
