1149a6ceda8f302693b7012848a4eff9ef85a964
cantinelli <cantinelli@beweb-16.176C>
2018-03-14 11:16:09 +0100

    Algo: Exercice10

--- src/com/beweb/lunel/programmation/exosEnVrac/AlgoMain.java
+++ src/com/beweb/lunel/programmation/exosEnVrac/AlgoMain.java
@@ -5,11 +5,7 @@
  */
 package com.beweb.lunel.programmation.exosEnVrac;
 
-import com.beweb.lunel.programmation.exosEnVrac.algo.Exercice5;
-import com.beweb.lunel.programmation.exosEnVrac.algo.Exercice1;
-import com.beweb.lunel.programmation.exosEnVrac.algo.Exercice2;
-import com.beweb.lunel.programmation.exosEnVrac.algo.Exercice3;
-import com.beweb.lunel.programmation.exosEnVrac.algo.Exercice7;
+import com.beweb.lunel.programmation.exosEnVrac.algo.Exercice10;
 
 /**
  *
@@ -21,10 +17,8 @@ public class AlgoMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Exercice1.launch();
-        Exercice2.launch();
-        Exercice3.launch();
-        Exercice7.launch();
+
+        Exercice10.launch();
         
     }
     
