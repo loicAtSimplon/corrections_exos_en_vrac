f714f1922121e2b17a3096070f3d41360507fc63
Yannis Piot Pilot <yannis@piotpilot.eu>
2018-03-14 09:23:36 +0100

    Add exo 5

--- src/com/beweb/lunel/programmation/exosEnVrac/AlgoMain.java
+++ src/com/beweb/lunel/programmation/exosEnVrac/AlgoMain.java
@@ -5,9 +5,11 @@
  */
 package com.beweb.lunel.programmation.exosEnVrac;
 
+import com.beweb.lunel.programmation.exosEnVrac.algo.Exercice5;
 import com.beweb.lunel.programmation.exosEnVrac.algo.Exercice1;
 import com.beweb.lunel.programmation.exosEnVrac.algo.Exercice2;
 import com.beweb.lunel.programmation.exosEnVrac.algo.Exercice3;
+import com.beweb.lunel.programmation.exosEnVrac.algo.Exercice4;
 
 /**
  *
@@ -22,6 +24,8 @@ public class AlgoMain {
         Exercice1.launch();
         Exercice2.launch();
         Exercice3.launch();
+        Exercice4.launch();
+        Exercice5.launch();
         
     }
     
---
+++ src/com/beweb/lunel/programmation/exosEnVrac/algo/Exercice5.java
@@ -0,0 +1,22 @@
+package com.beweb.lunel.programmation.exosEnVrac.algo;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author yannis
+ */
+public class Exercice5 {
+    public static void launch(){
+       int compteur = 10;
+       
+       while(compteur<=100){
+           System.out.println(compteur);
+           compteur++;
+       }
+    }
+}
